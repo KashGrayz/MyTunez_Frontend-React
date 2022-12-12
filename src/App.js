2a3669180e69da0d05c6dbe156ebff0ccd039ed7
Kash Grays <kasharsgrays93@.com>
2022-12-12 18:05:43 -0500

    GET Request functionality + Basic NavBar Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import Navbar from '../src/Components/Navbar/NavBar.jsx';
 
 
 function App() {
+
+  const[songs, setSongs] = useState([]);
+
+  useEffect(()=> {
+    getAllSongs();
+
+  }, [])
+
+  async function getAllSongs(){
+    let response = await axios.get('http://127.0.0.1:8000/api/songs/');
+    //console.log(response)
+    //debugger;
+    setSongs(response.data)
+  }
+
+
   return (
     <div>
-      <h1>MyTunez</h1>
+      <div>
+        <Navbar/>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Navbar/NavBar.jsx
@@ -0,0 +1,14 @@
+// Create NavBar for top of page.
+
+const Navbar = () => {
+    return(
+        <header>
+            <div>
+                <nav>
+                    <h1>My<small className="text-muted" style={{'color':'#BE0202'}}>Tunez</small></h1>
+                </nav>
+            </div>
+        </header>
+    )
+}
+export default Navbar;
